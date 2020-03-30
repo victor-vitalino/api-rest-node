eb8d1d1f0aeea4934aa9c7a03b7c9ade44dd1b3c
Victor Vitalino <victorvitalino237@gmail.com>
2020-03-30 09:28:46 -0300

    secret jwt movida para .env

--- src/auth/jwt.js
+++ src/auth/jwt.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken')
-const secret = 'jhdkjadhkadb5465&$%#'
+const secret = require('../../.env')
 
 // jwt.sign = recebe o objeto no qual sera gerado o token, a chave de validacao, e o tempo de expiração
 //jwt.verify = verifica se o token ainda é valido
 module.exports = {
     GenToken(obj) {
-        return jwt.sign(obj,secret,{expiresIn:84000})
+        return jwt.sign( obj, secret, {
+            expiresIn:84000 
+        })
     },
     Decode(token){
         return jwt.verify(token, secret)
